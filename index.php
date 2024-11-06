d360a85461777c39ced682329cb51d2f951543fa
walter-rodriguez-contreras <181468888+walter-rodriguez-contreras@users.noreply.github.com>
2024-11-06 11:40:22 +0100

    Upload index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    // include 'php_class/Contenido.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="cache-control" content="no-cache"/>
+        <title>Web Temp 2</title>
+    </head>
+    <body>
+        
+        <div>
+            <p>Parafo 1</p>
+        </div>
+
+    </body>
+</html>
+
+
